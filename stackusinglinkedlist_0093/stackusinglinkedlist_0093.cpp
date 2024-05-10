e2f95bcb6a5f8f96642f6127e59c090891f8bca5
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-10 16:44:55 +0700

    commit 2 input public dan int data dalam class node

--- stackusinglinkedlist_0093/stackusinglinkedlist_0093.cpp
+++ stackusinglinkedlist_0093/stackusinglinkedlist_0093.cpp
@@ -3,3 +3,13 @@ using namespace std;
 
 // NOde class reprenting a single node in the linked list
 class Node
+{
+public:
+    int data;
+    Node* next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
