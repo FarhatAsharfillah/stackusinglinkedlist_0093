08cb9f0ab498c86440b702b3e5eb96fbc601658c
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-10 17:01:14 +0700

    commit 18 input switch choice case 4

--- stackusinglinkedlist_0093/stackusinglinkedlist_0093.cpp
+++ stackusinglinkedlist_0093/stackusinglinkedlist_0093.cpp
@@ -115,3 +115,6 @@ int main()
                 cout << "Stack is Empty. No top value." << endl;
             }
             break;
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
